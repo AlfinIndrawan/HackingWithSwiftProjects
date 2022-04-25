c108f4a0f68716877e41845c2c82a63b795fc930
AlfinIndrawan <alfinindrawan54@gmail.com>
2022-04-25 16:14:52 +0700

    day4

--- Project2(GuessFlag)/Project2(GuessFlag)/ViewController.swift
+++ Project2(GuessFlag)/Project2(GuessFlag)/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet var button3: UIButton!
     var countries = [String]()
     var score = 0
+    var correctAnswer = 0
     override func viewDidLoad() {
         super.viewDidLoad()
 //        countries.append("estonia")
@@ -38,13 +39,31 @@ class ViewController: UIViewController {
         askQuestion()
   }
     func askQuestion(action: UIAlertAction! = nil) {
+        countries.shuffle()
+        correctAnswer=Int.random(in: 0...2)
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
-        
+        title = countries[correctAnswer].uppercased()
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
             // dispose memory
+    }
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        }
+        else {
+            title = "Wrong"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your Score is \(score)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction( title: "Continue", style: .default, handler: askQuestion))
+        present(ac, animated: true)
     }
 }
