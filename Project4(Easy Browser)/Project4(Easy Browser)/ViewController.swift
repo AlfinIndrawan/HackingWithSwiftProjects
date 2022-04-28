238e5efea51b250a8a29a89b6a80b662499d15fb
AlfinIndrawan <alfinindrawan54@gmail.com>
2022-04-28 21:31:45 +0700

    Day6

--- Project4(Easy Browser)/Project4(Easy Browser)/ViewController.swift
+++ Project4(Easy Browser)/Project4(Easy Browser)/ViewController.swift
@@ -9,7 +9,8 @@ import UIKit
 
 class ViewController: UIViewController, WKNavigationDelegate /*protocol*/ {
     var webView : WKWebView!
-    
+    var progressView : UIProgressView!
+    var websites = ["apple.com", "hackingwithswift.com"]
     override func loadView() {
         webView = WKWebView() //web renderer
         webView.navigationDelegate = self //when any web page navigation happens, please tell me – the current view controller.
@@ -18,31 +19,61 @@ class ViewController: UIViewController, WKNavigationDelegate /*protocol*/ {
     }
     
     override func viewDidLoad() {
+        super.viewDidLoad()
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
         
-        super.viewDidLoad()
-        let url = URL(string: "https://www.hackingwithswift.com")! // must have https
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        
+        progressView = UIProgressView(progressViewStyle: .default)
+        progressView.sizeToFit()
+        let progressButton = UIBarButtonItem(customView: progressView) //customview will make smaller
+        
+        toolbarItems = [progressButton,spacer,refresh]
+        navigationController?.isToolbarHidden = false
+        
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress),options: .new, context: nil)
+        
+        let url = URL(string: "https://" + websites[0])! // must have https
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
     }
-    
+
     @objc func openTapped() {
         let ac = UIAlertController(title: "Open Page...", message: nil, preferredStyle: .actionSheet)
-        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
-        ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage))
+        for website in websites {
+            ac.addAction(UIAlertAction(title: website, style: .default, handler: openPage))
+        }
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
-         present(ac, animated: true)
+        present(ac, animated: true)
     }
     func openPage(action: UIAlertAction) {
         guard let actionTitle = action.title else {return}
         guard let url = URL(string: "https://" + actionTitle) else {return}
         webView.load(URLRequest(url: url))
     }
+    
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         title = webView.title
-    } // called when webciew finish
-
-
+    } // called when webview finish
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            progressView.progress = Float(webView.estimatedProgress)
+        } //kvo implementation
+    }
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) /* escaping for the clousre might be used later on */ {
+        let url = navigationAction.request.url
+        if let host = url?.host {
+            for website in websites {
+                if host.contains(website) {
+                    decisionHandler(.allow) //if website found closure allow loading
+                    return
+                }
+            }
+        }
+        decisionHandler(.cancel) // if url dont have host
+    }
 }
 
