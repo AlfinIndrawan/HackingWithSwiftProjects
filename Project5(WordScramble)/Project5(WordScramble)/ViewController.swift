d02a66ad9a3f75f0a91a7fef2abf0956472c68de
AlfinIndrawan <alfinindrawan54@gmail.com>
2022-04-30 19:15:43 +0700

    day 8

--- Project5(WordScramble)/Project5(WordScramble)/ViewController.swift
+++ Project5(WordScramble)/Project5(WordScramble)/ViewController.swift
@@ -40,7 +40,7 @@ class ViewController: UITableViewController {
         cell.textLabel?.text = usedWords[indexPath.row]
         return cell
     }
-    @objc func promptAnwer () { // call anser
+    @objc func promptAnswer () { // call anser
         let ac = UIAlertController(title: "enter answer", message: nil, preferredStyle: .alert)
         ac.addTextField() //add new textbox to ui controller
         
@@ -54,8 +54,57 @@ class ViewController: UITableViewController {
         present(ac, animated: true)
     }
     func submit(_ answer: String) {
+        let lowerAnswer = answer.lowercased()
+        let errorTitle: String
+        let errorMessage: String
+        if isPossible(word: lowerAnswer) {
+            if isOriginal (word: lowerAnswer) {
+                if isReal(word: lowerAnswer) {
+                    usedWords.insert(answer, at: 0)
+                    
+                    let indexPath = IndexPath(row: 0, section: 0)
+                    tableView.insertRows(at: [indexPath], with: .automatic)
+                    return // exit so it will not run ac
+                } else {
+                    errorTitle = "Word not recognized"
+                    errorMessage = "You can't just make them up"
+                }
+            } else {
+                errorTitle = "Word already used"
+                errorMessage = "Be more original"
+            }
+        } else {
+            errorTitle = "Word not possible"
+            errorMessage = "You cant spell that from \(title!.lowercased())."
+        }
         
+        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default ))
+        present(ac, animated: true)
     }
     
+    func isPossible(word: String) -> Bool {
+        guard var tempWord = title?.lowercased() else { return false }
+        for letter in word {
+            if let position = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: position)
+            }
+            else {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        return !usedWords.contains(word)
+    }
+    func isOriginal(word: String) -> Bool {
+        let checker = UITextChecker ()
+        let range = NSRange(location: 0, length: word.utf16.count) //uikit use utf16 because written in obj c use in spritekit and other kit
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en") // look at the first paramater and second other is just to complete
+        return misspelledRange.location == NSNotFound // (NSNotfound telling you word is spelled correctly )
+         
+    }
 }
 
